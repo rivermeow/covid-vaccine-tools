8a199a5fef1f28f5e76aca095e5ce6f7c255c85e
rivermeow <79930312+rivermeow@users.noreply.github.com>
2021-05-03 10:02:33 -0700

    Update riteaid-status.js
    Updated to reflect response changes

--- bookmarklets/riteaid-status.js
+++ bookmarklets/riteaid-status.js
@@ -13,7 +13,7 @@ javascript:
 
 		$.get( CHECK_SLOTS_URL, { storeNumber: num}, function( data ) {
 		  
-		  if(data.Data.slots[1] == true){
+		  if(hasSlots(data.Data.slots) == true){
 			checkForSlotTimes(num, store);
 		  } else {
 			getStoreIcon(store).append('<span title="No appointments are available" class="appt-check-status"><br/><i class="fas fa-times-circle fa-2x"></i></span>');
@@ -22,14 +22,33 @@ javascript:
 			
 	});
 
+	function hasSlots(slots){
+	    var found = false;
+        for (const [key, value] of Object.entries(slots)) {
+            if(value == true || value.length > 0){found = true;break;}; 
+        }
+        return found;
+	}
+
+	function firstNotEmpty(slots){
+	    var found = false;
+        for (const [key, value] of Object.entries(slots)) {
+            if(value.length > 0){found = true;return key;}; 
+        }
+        return -1;
+	}
+
 	function checkForSlotTimes(num, store ){
 			grecaptcha.execute(sitekey, {action: 'submit'}).then(function(mytoken) {
 				console.log(mytoken);
 				$.get( GET_SLOTS_URL, { storeNumber: num, captchatoken: mytoken, moment:new Date().getTime()}, function( data ) {
-				  if(data.Data.slots[1].length > 0 == true){
+				  if(hasSlots(data.Data.slots)){
 					console.log('store ' + num + ' may have appointments: ' + data.Data.slots[1]);
 					getStoreIcon(store).append('<span title="Appointsment may be available" class="appt-check-status"><br/><i class="fas fa-check-circle fa-2x" style="color:green;"></i></span>');
-					$(store).find('.covid-store__store__btn').append('<span title="'+getDayBreakdown(data.Data.slots[1])+'"class="slots-found link color-black">'+data.Data.slots[1].length+' slots found: ' + getDayBreakdown(data.Data.slots[1]) + '</span>');				
+					var breakdown = getDayBreakdown(data.Data.slots);
+					console.log(breakdown);
+					$(store).find('.covid-store__store__btn').append('<span title="'+breakdown+
+					 '"class="slots-found link color-black">'+ 'Slots found: ' + breakdown + '</span>');				
 				  } else {
 					getStoreIcon(store).append('<span title="No appointments are available" class="appt-check-status"><br/><i class="fas fa-times-circle fa-2x"></i></span>');
 				  }
@@ -39,12 +58,14 @@ javascript:
 
 	function getDayBreakdown(slots) {
 		var counts = {};
-		slots.forEach(function(item){
+		var index = firstNotEmpty(slots);
+		slots[index].forEach(function(item){
 			var day = item.substring(0,item.indexOf('T'));
 			counts[day] = counts[day] ? counts[day] + 1 : 1;
 		});
 		
 		var countList = '';
+		
 		Object.entries(counts).forEach(function(v,k){countList += '<br/>' + v[0] + ' - ' + v[1];});
         countList = '<span>' + countList + '</span>';
 		
